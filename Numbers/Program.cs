8fcc40169211e069d253546520368fa414cc1899
jamalcow <james.thom.business@gmail.com>
2025-07-26 00:57:17 +1000

    end chapter 2

---
+++ Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿// An unsigned integer is a positive whole number or 0.
+uint naturalNumber = 23;
+// An unsigned integer is a negative whole number or 0.
+int integerNumber = -23;
+
+// A float is a single precision floating point number.
+// The F or f suffix makes the value a float literal.
+// The suffix is required to compile.
+float realNumber = 2.3f;
+
+// A double is a double precision floating point number.
+// double is the default for a number value with a decimal point.
+double anotherRealNumber = 2.3;  // a double literal value.
+
+int Decim
