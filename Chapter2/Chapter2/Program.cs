aca38319cef216f5a5b1524c49e1274469434fd6
jamalcow <james.thom.business@gmail.com>
2025-07-26 00:11:48 +1000

    Vocabulary.cs

---
+++ Chapter2/Chapter2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Chapter2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // #error version
+
+        }
+    }
+}
---
+++ Vocabulary/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+// #error version
+// WriteLine($"Computer Named {Env.MachineName} says \"No.\"");
+using System.Reflection;
+
+System.Data.DataSet ds = new();
+HttpClient client = new();
+
+// Get the assembly that is the entry point of the application.
+Assembly? myApp = Assembly.GetEntryAssembly();
+
+// If null, close program.
+if (myApp is null) return;
+
+// Loop through the assemblies the my app references.
+foreach (AssemblyName name in myApp.GetReferencedAssemblies())
+{
+    // Load the assembly so we can read its details.
+    Assembly a = Assembly.Load(name);
+
+    // Declare a variable to count the number of methods.
+    int methodCount = 0;
+
+    // Loop through the types in the assembly.
+    foreach (TypeInfo t in a.DefinedTypes)
+    {
+        // Add upp the counts of all the methods.
+        methodCount += t.GetMethods().Length;
+    }
+
+    // Output the count of types and their different methods.
+    WriteLine("{0:N0} types with {1:N0} methods in {2} assembly.",
+        arg0: a.DefinedTypes.Count(),
+        arg1: methodCount,
+        arg2: name.Name);
+}
